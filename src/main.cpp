824f5c63a51409b7b93b731d71e664ea7186af18
Kshitij <kpg284@nyu.edu>
2025-03-19 18:38:29 +0100

    sanity check: arguments are passed correctly

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,25 @@
 #include <mbed.h>
 
+extern "C" uint32_t fibonacci(uint32_t n);
+
 int main()
 {
-    uint32_t n = 12;
+    uint32_t n = 10;
     while (1)
     {
-        printf("The input to the function is %d", n);
+        printf("The input to the function is %ld \n", n);
+
+        // Sanity check, passing 10 to the function to check if the assembly function receive the input currectly
+
+        uint32_t result = fibonacci(n);
+
+        if (result == 1)
+        {
+            printf("Works \n");
+        }
+        else
+        {
+            printf("Failed \n");
+        }
     }
 }

d6790f898e1b179353783d2208661e3f2ee7e443
Kshitij <kpg284@nyu.edu>
2025-03-20 08:36:49 +0100

    using stack to store intermediate values.

--- src/main.cpp
+++ src/main.cpp
@@ -3,15 +3,25 @@
 extern "C" uint32_t fibonacci(uint32_t n);
 extern "C" uint32_t factorial(uint32_t n);
 
+#define N 11
+
 int main()
 {
-    uint32_t n = 39;
+    uint32_t n = 9;
+
     while (1)
     {
         printf("The input to the function is %ld \n", n);
 
-        uint32_t result = factorial(n);
-
-        printf("factorial(%ld) = %ld \n", n, result);
+        //  Result can only be stored till 11! (using uint32_t for result.
+        if (n > N)
+        {
+            printf("The argument is too charge.)");
+        }
+        else
+        {
+            uint32_t result = factorial(n);
+            printf("factorial(%ld) = %ld \n", n, result);
+        }
     }
 }

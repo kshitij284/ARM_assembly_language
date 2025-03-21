93962b56172be120207ea08b586294c96580a033
Kshitij <kpg284@nyu.edu>
2025-03-21 14:26:40 +0100

    Implemented Inserstion_sort

--- src/main.cpp
+++ src/main.cpp
@@ -2,20 +2,21 @@
 
 extern "C" uint32_t fibonacci(uint32_t n);
 extern "C" uint32_t factorial(uint32_t n);
-extern "C" void bubble_sort(uint32_t* arr, uint32_t size);
+extern "C" void bubble_sort(uint32_t *arr, uint32_t size);
+extern "C" void insertion_sort(uint32_t *arr, uint32_t size);
 
 #define N 9
 
 int main()
 {
-    uint32_t array[N] = {9,2,5,3,8,4,1,6,7};
+    uint32_t array[N] = {9, 2, 5, 3, 8, 4, 1, 6, 7};
     while (1)
     {
-        bubble_sort(array, N);
-        for (int i = 0; i<N; i++){
-            printf("%ld ,",array[i]);
+        insertion_sort(array, N);
+        for (int i = 0; i < N; i++)
+        {
+            printf("%ld ,", array[i]);
         }
         printf("\n");
-        
     }
 }
